9c1ad66eb88ecb4059ba4bce19f2e247a74f54f5
adamclayton <egock@netscape.net>
2017-08-13 23:33:55 +1000

    Added changes

--- scraper.py
+++ scraper.py
@@ -1,24 +1,57 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+import logging
+import scraperwiki
+import sqlite3
+import time
+import urllib2
+import lxml.html
+from splinter import Browser
+
+def getrecord(html):
+  root = lxml.html.fromstring(html)
+  appid=root.xpath('//table[1]//tr[(@class="alternateRow") or (@class="normalRow")]/td[1]/a//text()')
+  dadate=root.xpath('//table[1]//tr[(@class="alternateRow") or (@class="normalRow")]/td[2]//text()')
+  descr=root.xpath('//tr[not(@class="pagerRow")]/td[3]//text()')
+  addr=root.xpath('//tr[not(@class="pagerRow")]/td[6]/a//text()')
+  info_url="https://eservices.lithgow.nsw.gov.au/ePropertyProd/P1/eTrack/eTrackApplicationDetails.aspx?r=P1.WEBGUEST&f=%24P1.ETR.APPDET.VIW&ApplicationId="
+  record={}
+  for index in range(len(appid)):
+    record['council_reference'] = appid[index]
+    record['description'] = descr[index]
+    record['address'] = addr[index]
+    record['date_received'] = dadate[index]
+    record['info_url']=info_url + appid[index]
+    record['comment_url']='mailto:council@lithgow.nsw.gov.au?subject=Development%20Application%20Enquiry%20' + appid[index]
+    try:
+      if scraperwiki.sqlite.select('* FROM data WHERE council_reference="%s"' % record['council_reference']):
+	logging.info('Skipping existing record in sqlite: ' + record['council_reference'])
+	continue
+    except sqlite3.OperationalError, e:
+      if 'no such table:' in e.message:
+	logging.info('Sqlite data does not exist yet. Will be created.')
+	pass
+      else:
+	raise
+
+    scraperwiki.sqlite.save(unique_keys=['council_reference'], data=record)
+
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+scrape_url='https://eservices.lithgow.nsw.gov.au/ePropertyProd/P1/eTrack/eTrackApplicationSearchResults.aspx?Field=S&Period=L7&r=P1.WEBGUEST&f=%24P1.ETR.SEARCH.SL7'
+
+# browser
+with Browser('phantomjs', load_images=False) as browser:
+  browser.visit(scrape_url)
+  #pages=browser.find_by_tag('a')
+  #for p in pages:
+  #  print p['href']
+  html=browser.html
+  getrecord(html)
+  browser.click_link_by_partial_href('Page$2')
+  html=browser.html
+  getrecord(html)
