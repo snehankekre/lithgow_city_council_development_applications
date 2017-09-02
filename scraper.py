0e350c5bf8d38f0286f96ceb7a8a07c9bc3ee8aa
adamclayton <egock@netscape.net>
2017-09-02 10:37:44 +1000

    Added column changes

--- scraper.py
+++ scraper.py
@@ -9,8 +9,10 @@ import time
 import urllib2
 import lxml.html
 from splinter import Browser
+from selenium.common.exceptions import TimeoutException
 
 def getrecord(html):
+  date_scraped = time.strftime('%Y-%m-%d')
   root = lxml.html.fromstring(html)
   appid=root.xpath('//table[1]//tr[(@class="alternateRow") or (@class="normalRow")]/td[1]/a//text()')
   dadate=root.xpath('//table[1]//tr[(@class="alternateRow") or (@class="normalRow")]/td[2]//text()')
@@ -22,9 +24,10 @@ def getrecord(html):
     record['council_reference'] = appid[index]
     record['description'] = descr[index]
     record['address'] = addr[index]
-    record['date_received'] = dadate[index]
+    record['date_received'] = time.strftime('%Y-%m-%d', time.strptime(dadate[index], '%d/%m/%Y'))
     record['info_url']=info_url + appid[index]
     record['comment_url']='mailto:council@lithgow.nsw.gov.au?subject=Development%20Application%20Enquiry%20' + appid[index]
+    record['date_scraped']=date_scraped
     try:
       if scraperwiki.sqlite.select('* FROM data WHERE council_reference="%s"' % record['council_reference']):
 	logging.info('Skipping existing record in sqlite: ' + record['council_reference'])
@@ -35,22 +38,32 @@ def getrecord(html):
 	pass
       else:
 	raise
-
     scraperwiki.sqlite.save(unique_keys=['council_reference'], data=record)
 
 
 
-#logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 scrape_url='https://eservices.lithgow.nsw.gov.au/ePropertyProd/P1/eTrack/eTrackApplicationSearchResults.aspx?Field=S&Period=L7&r=P1.WEBGUEST&f=%24P1.ETR.SEARCH.SL7'
 
-# browser
+# fix data (only run these lines once)
+#scraperwiki.sqlite.execute('alter table data add column date_scraped text')
+#scraperwiki.sqlite.execute("update data set date_received=replace(date_received,'-','/')")
+#scraperwiki.sqlite.execute("update data set date_scraped=date_received")
+#print scraperwiki.sqlite.select('* FROM data')
+
+# browser/scraper
 with Browser('phantomjs', load_images=False) as browser:
-  browser.visit(scrape_url)
-  html=browser.html
-  getrecord(html)
-  links=browser.find_link_by_partial_href('Page$2')
-  if (links.is_empty()==False):
-    browser.click_link_by_partial_href('Page$2')
+  browser.driver.set_page_load_timeout(600)
+  try:
+    browser.visit(scrape_url)
     html=browser.html
     getrecord(html)
+    links=browser.find_link_by_partial_href('Page$2')
+    if (links.is_empty()==False):
+      browser.click_link_by_partial_href('Page$2')
+      html=browser.html
+      getrecord(html)
+  except TimeoutException:
+    logging.info('Process timed out')
+    pass
