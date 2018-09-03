08147fc25fb11cafb7bdc4848436d3ffa483d968
Snehan Kekre <snehan@minerva.kgi.edu>
2018-09-03 14:42:31 +0545

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -59,7 +59,7 @@ scrape_url='https://eservices.lithgow.nsw.gov.au/ePropertyProd/P1/eTrack/eTrackA
 
 
 # browser/scraper
-with Browser('chrome') as browser:
+with Browser() as browser:
   browser.driver.set_page_load_timeout(300)
   try:
     browser.visit(scrape_url)
